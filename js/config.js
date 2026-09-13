b8c0163888d86a49efbcc8726b92f5eafdb770c9
admin-ktms <admin.ktms@gmail.com>
2026-09-13 23:25:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,21 @@
 
+export const CONFIG = Object.freeze({
+
+  APP_NAME: "KTMS",
+
+  API_BASE_URL:
+    "https://gfokwfsqsttrjueqjojz.supabase.co/functions/v1/ktms-public-api",
+
+  API_VERSION:
+    "v1",
+
+  SESSION_STORAGE_KEY:
+    "ktms_player_session",
+
+  PENDING_ROUTE_KEY:
+    "ktms_pending_route",
+
+  PENDING_TOURNAMENT_KEY:
+    "ktms_pending_tournament"
+
+});
